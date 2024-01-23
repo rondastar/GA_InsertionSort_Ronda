ea9b686b1b1e40c970d9390dc8faa4749f3018ff
rondastar <116147793+rondastar@users.noreply.github.com>
2024-01-22 17:21:51 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_InsertionSort_Ronda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
